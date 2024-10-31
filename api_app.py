b3fc9cbd30a2e2f2736cf0d972cb2be22580dbf0
박준현 <gktkwk91@naver.com>
2024-10-31 12:46:08 +0900

    REFACTOR : 서버 API 메소드재정의 및구조  수정

--- api_app.py
+++ api_app.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Depends
 from fastapi.middleware.cors import CORSMiddleware
 import os
 from dotenv import load_dotenv
-from service.ocr_service import *
-from service.txt_to_speech import *
-from service.tran_service import *
-from service.sum_service import *
+from usecase.image_usecase import *
+from usecase.text_usecase import *
+from pydantic import BaseModel
+from fastapi.encoders import jsonable_encoder
 
 # .env 파일의 환경 변수 불러오기
 load_dotenv()
@@ -17,6 +17,15 @@ translate_service = TransService()
 sum_service = SummaryService()
 
 app = FastAPI()
+
+class TextRequest(BaseModel):
+    text: str
+
+class ImageRequest(BaseModel):
+    from_lang: int
+    to_lang: int
+    is_summary: bool
+    is_speech: bool
 
 # 허용할 도메인 목록 설정
 origins = [
@@ -32,63 +41,30 @@ app.add_middleware(
     allow_methods=["*"],          # 모든 HTTP 메소드 허용
     allow_headers=["*"],          # 모든 HTTP 헤더 허용
 )
-# 텍스트,이미지 : 내용, 요약, 음성
 
-#1. 이미지를 뽑아 내용을 요약하고 번역해서 읽어준다. -텍스트
-@app.post("/ocr-sum-spc")
-async def create_speech(file:UploadFile, lang: str, tran_lang:str):
-    contents = await file.read()
-
-    ocr_result = OcrService.ocr_text(contents, lang)
-    sum_result = sum_service.get_summary(ocr_result)
-    tran_result = translate_service.get_translation(sum_result)
-    speech_result = TxtSpeech.get_speech(tran_result, lang)
-    
-    return {
-        "result": speech_result
-        }   
-
-#2. 이미지를 뽑아 내용을 번역해서 읽어준다. -요약 -텍스트
-@app.post("/ocr-spc")
-async def create_speech(file:UploadFile, lang: str, tran_lang:str):
+# 파일은 별도로 받고
+@app.post("/images")
+async def create_speech(file: UploadFile,  request: ImageRequest = Depends()): # JSON 데이터는 요청 바디로 받기):
+    image_usecase = ImageUsecase()
     contents = await file.read()
     
-    ocr_result = OcrService.ocr_text(contents, lang)
-    tran_result = translate_service.get_translation(ocr_result)
-    speech_result = TxtSpeech.get_speech(tran_result, tran_lang)    
-
-    return {
-        "result": speech_result
-        }
-#3. 이미지를 뽑아 내용을 번역해서 읽어준다. -요약 -음성
-@app.post("/ocr-tran")
-async def create_speech(file:UploadFile, lang: str, tran_lang:str):
-    contents = await file.read()
+    # ImageUsecase의 excute 메서드에 JSON 데이터를 전달
+    speech_result = image_usecase.excute(
+        contents, 
+        request.from_lang, 
+        request.to_lang, 
+        request.is_summary, 
+        request.is_speech
+    )
     
-    ocr_result = OcrService.ocr_text(contents, lang)
-    tran_result = translate_service.get_translation(ocr_result) 
+    return {"result": speech_result}
 
-    return {
-        "result": tran_result
-        }
+@app.post("/text")
+async def create_text(request: TextRequest, from_lang:int, to_lang:int, is_summary:bool, is_speech:bool):
+    text_jonson_data = jsonable_encoder(request)
+    text_usecase = TextUsecase()
+    tran_result = text_usecase.excute(text_jonson_data['text'], from_lang, to_lang, is_summary, is_speech)
 
-#4. 텍스트 내용을 번역한다. -이미지 -요약 -음성
-@app.post("/txt")
-async def create_text(text: str, lang):
-    # 번역 언어 선택을 좀 더 자유롭게 하도록 수정
-    tran_result = translate_service.get_translation(text)
-
-    return {
-        "result" : tran_result
-    }
-
-#5. 텍스트 내용을 요약하고 번역한다. -이미지 -음성
-@app.post("/txt-sum")
-async def create_file(text: str):
-    
-    sum_result = sum_service.get_summary(text)
-    tran_result = translate_service.get_translation(sum_result)
-    
     return {
         "result" : tran_result
     }
--- service/spc_service.py
+++ service/spc_service.py
@@ -1,7 +1,8 @@
 from models.tt_speech.spc_detector import SpcDetector
 
-class TxtSpeech():
-
+class SpeechService():
+    
+    @staticmethod
     def get_speech(text:str, lang:str):
         return SpcDetector.detection(text, lang)
     
