9612c891dba42d525a593223e0f5a76eb64e4f3c
박준현 <gktkwk91@naver.com>
2024-11-04 16:38:02 +0900

    CHORE : 번역 모델 이름 수정

--- models/tt_tran/tran_detector.py
+++ models/tt_tran/tran_detector.py
@@ -7,7 +7,7 @@ class TranDetector():
     def detection(self, text:str, from_lang:str, to_lang:str):
         # 언어 선택을 좀 더 자유롭게 하기
         #step4
-        translator = pipeline('translation', model='facebook/nllb-200-distilled-600M', device=-1, src_lang=from_lang, tgt_lang=to_lang, max_length=512)
+        translator = pipeline('translation', model= self.model_name, device=-1, src_lang=from_lang, tgt_lang=to_lang, max_length=512)
 
         #step5
         result = translator(text, max_length=512)
