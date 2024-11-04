58bc4f8c9aba33a56b2cfcc076915c494baf191c
박준현 <gktkwk91@naver.com>
2024-11-04 16:13:44 +0900

    PERF : 요약 모델 및 번역 모델 성능 걔선

--- models/tt_sum/summary_detector.py
+++ models/tt_sum/summary_detector.py
@@ -1,21 +1,32 @@
 
 from transformers import pipeline
+import nltk
+nltk.download('punkt')
+nltk.download('punkt_tab')
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
 
 class SummaryDetector():
     def __init__(self):
-        self.summarizer = None
+        self.model = None
 
     def get_detector(self):
-        if self.summarizer == None:
-            self.summarizer = pipeline(
-                "summarization",
-                model="stevhliu/my_awesome_billsum_model")
-        
-        return self.summarizer
+        if self.model == None:
+            self.model = AutoModelForSeq2SeqLM.from_pretrained('eenzeenee/t5-base-korean-summarization')
+    
+        return self.model
     
     
     def detection(self, text: str):
-        summarizer =self.get_detector()
-        summary = summarizer(text)
+        model =self.get_detector()
+        tokenizer = AutoTokenizer.from_pretrained('eenzeenee/t5-base-korean-summarization')
 
-        return summary[0]['summary_text']
+        prefix = "summarize: "
+        sample = text
+        inputs = [prefix + sample]
+
+        inputs = tokenizer(inputs, max_length=512, truncation=True, return_tensors="pt")
+        output = model.generate(**inputs, num_beams=3, do_sample=True, min_length=10, max_length=512)
+        decoded_output = tokenizer.batch_decode(output, skip_special_tokens=True)[0]
+
+        return nltk.sent_tokenize(decoded_output.strip())[0]
--- service/tran_service.py
+++ service/tran_service.py
@@ -6,8 +6,7 @@ class TransService():
         self.tran_detector = TranDetector()
     
     def get_translation(self, text:str, from_lang:str, to_lang:str):
-        model = self.tran_detector.get_model()
-        tokenizer = self.tran_detector.get_tokenizer()
         translate_result = self.tran_detector.detection(text, from_lang, to_lang)
 
         return translate_result
+
